1ff7bb06758d581780d11e2e3673564f223307a5
Antonio De Vivo <antonio.devivo@yahoo.com>
2026-07-25 14:43:22 +0200

    chore: rename

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 """Entrypoint del modulo.
 
 Uso:
-    python -m src.main --pdfs2md
+    python -m src.main --pdf2md
 """
 
 import argparse
-from src.scripts.pdf2md import convert_all_pdfs
+from scripts.pdf2md import convert_all_pdfs
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Utility del modulo.")
     parser.add_argument(
-        "--pdfs2md",
+        "--pdf2md",
         action="store_true",
         help="Converte tutti i PDF in docs/pdf in file Markdown in docs/md.",
     )
@@ -21,7 +21,7 @@ def parse_args() -> argparse.Namespace:
 def main() -> None:
     args = parse_args()
 
-    if args.pdfs2md:
+    if args.pdf2md:
         convert_all_pdfs()
 
 
